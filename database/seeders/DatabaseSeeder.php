c541583e5c6dfd235916e36a2f2929217896e43d
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2021-09-13 10:45:38 +0300

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //Create Divisions
+        $path = base_path('database/seeders/divisions.sql');
+        $this->command->info("Seeding $path");
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Divisions Seeded!');
+
+        //Create Cities
+        $path = base_path('database/seeders/cities.sql');
+        $this->command->info("Seeding $path");
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Cities Seeded!');
+
+        //Create Categories
+        $path = base_path('database/seeders/category.sql');
+        $this->command->info("Seeding $path");
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Categories Seeded!');
+
+        //Create SubCategories
+        $path = base_path('database/seeders/sub_category.sql');
+        $this->command->info("Seeding $path");
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Sub Categories Seeded!');
+
+        $this->command->info("Emptying Tables..");
+        DB::table("users")->truncate();
+        DB::table("posts")->truncate();
+        DB::table("postimages")->truncate();
+
+        $this->command->info("Emptying Image Folders..");
+        /* Delete previous images*/
+        $files = glob('public/images/*');
+        foreach ($files as $file) {
+            if (is_file($file))
+                unlink($file);
+        }
+        /* Delete previous thumbnails*/
+        $files = glob('public/images/thumb/*');
+        foreach ($files as $file) {
+            if (is_file($file))
+                unlink($file);
+        }
     }
 }
